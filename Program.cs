83c02703be561a5cf92850957f8ece57beb9482b
Your Name <you@example.com>
2021-01-10 21:54:35 -0300

    Exercicio2

---
+++ Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace funcoes2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+           
+            Console.WriteLine("Digite a idade do ator ou atriz");
+            int idade = int.Parse(Console.ReadLine());
+
+
+            while (idade != 35)    
+            {
+               Console.WriteLine("Você Errou");
+               Console.WriteLine("Digite a idade do ator ou atriz novamente");
+               idade = int.Parse(Console.ReadLine());
+
+            }    
+
+
+                Console.WriteLine("Parabéns você acertou");
+                
+
+        }
+    }
+}
